6dc57ae3eaa1d6fb8d9eaf4e51270b21ad19e4ba
manikpanwar <mpanwar@andrew.cmu.edu>
2014-11-01 00:11:44 -0400

    test

--- node/routes/index.js
+++ node/routes/index.js
@@ -7,7 +7,11 @@ router.get('/', function(req, res) {
 });
 
 router.get('/newuser',function(req,res){
-    res.render('newuser',{title: 'Add new user'})
+    res.render('newuser',{title: 'Add new user'});
+});
+
+router.get('/newpost',function(req,res){
+    res.render('newpost',{title: 'Add new post'});
 });
 
 router.get('/userlist',function(req,res){
@@ -20,15 +24,39 @@ router.get('/userlist',function(req,res){
     });
 });
 
-router.post('/adduser',function(req,res){
+router.post('/addpost',function(req, res){
     var db = req.db;
-
     var userName = req.body.username;
     var userLocation = req.body.userlocation;
     var userEmail = req.body.useremail;
+    var posts = db.get("posts");
+    var timeUserWaits = req.body.timetoeat;
 
+    posts.insert({
+        "username": userName,
+        "userlocation": userLocation,   
+        "timeuserwaits": timeUserWaits,
+        "useremail": userEmail
+    },function(err,doc){
+        if (err) {
+            // If it failed, return error
+            res.send("There was a problem adding the information to the database.");
+        }
+        else {
+            // If it worked, set the header so the address bar doesn't still say /adduser
+            res.location("userlist");
+            // And forward to success page
+            res.redirect("userlist");
+        }
+    });
+});
+
+router.post('/adduser',function(req,res){
+    var db = req.db;
+    var userName = req.body.username;
+    var userLocation = req.body.userlocation;
+    var userEmail = req.body.useremail;
     var users = db.get("users");
-
     var timeUserWaits = req.body.timetoeat;
 
     users.insert({
