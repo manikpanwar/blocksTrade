288657b65a3f415121d40273444222e0b93402b9
germy <germyjermy@yahoo.com>
2014-10-31 22:47:40 -0400

    fixed userlist

--- node/routes/index.js
+++ node/routes/index.js
@@ -50,10 +50,12 @@ router.post('/adduser',function(req,res){
 
 });
 
+module.exports = router;
+
 /* GET Userlist page. */
 router.get('/userlist', function(req, res) {
     var db = req.db;
-    var collection = db.get('usercollection');
+    var collection = db.get('users');
     collection.find({},{},function(e,docs){
         res.render('userlist', {
             "userlist" : docs
@@ -61,6 +63,6 @@ router.get('/userlist', function(req, res) {
     });
 });
 
-module.exports = router;
+
 
 
