ac7d7d246566872af485c0afdbcb107925b02017
manikpanwar <mpanwar@andrew.cmu.edu>
2014-10-31 21:28:59 -0400

    pushing onto database working

--- node/routes/index.js
+++ node/routes/index.js
@@ -6,6 +6,50 @@ router.get('/', function(req, res) {
   res.render('index', { title: "CMU's Block Trader" });
 });
 
+router.get('/newuser',function(req,res){
+    res.render('newuser',{title: 'Add new user'})
+});
+
+router.get('/userlist',function(req,res){
+    var db = req.db;
+    var collection = db.get('users');
+    collection.find({},{},function(e,docs){
+        res.render('userlist', {
+            "userlist" : docs
+        });
+    });
+});
+
+router.post('/adduser',function(req,res){
+    var db = req.db;
+
+    var userName = req.body.username;
+    var userLocation = req.body.userlocation;
+
+    var users = db.get("users");
+
+    var timeUserWaits = req.body.timetoeat;
+
+    users.insert({
+        "username": userName,
+        "userlocation": userLocation,
+        "timeuserwaits": timeUserWaits
+    },function(err,doc){
+        if (err) {
+            // If it failed, return error
+            res.send("There was a problem adding the information to the database.");
+        }
+        else {
+            // If it worked, set the header so the address bar doesn't still say /adduser
+            res.location("userlist");
+            // And forward to success page
+            res.redirect("userlist");
+        }
+    });
+
+
+});
+
 module.exports = router;
 
 /* GET Userlist page. */
